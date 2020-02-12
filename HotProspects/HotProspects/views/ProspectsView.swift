dd10cafeff3e06a854c405433d017f028be9d1d6
yihwan <hi@yihwan.kim>
2020-02-11 20:58:31 -0800

    add day 83

--- HotProspects/HotProspects/views/ProspectsView.swift
+++ HotProspects/HotProspects/views/ProspectsView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 enum FilterType {
     case none, contacted, uncontacted
@@ -14,6 +15,8 @@ enum FilterType {
 
 struct ProspectsView: View {
     @EnvironmentObject var prospects: Prospects
+    
+    @State private var isShowingScanner = false
     
     let filter: FilterType
     
@@ -49,18 +52,40 @@ struct ProspectsView: View {
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
                     }
+                    .contextMenu {
+                        Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted" ) {
+                            self.prospects.toggle(prospect)
+                        }
+                    }
                 }
             }
-                .navigationBarTitle(title)
-                .navigationBarItems(trailing: Button(action: {
-                    let prospect = Prospect()
-                    prospect.name = "Paul Hudson"
-                    prospect.emailAddress = "paul@hackingwithswift.com"
-                    self.prospects.people.append(prospect)
-                }) {
-                    Image(systemName: "qrcode.viewfinder")
-                    Text("Scan")
-                })
+            .navigationBarTitle(title)
+            .navigationBarItems(trailing: Button(action: {
+                self.isShowingScanner = true
+            }) {
+                Image(systemName: "qrcode.viewfinder")
+                Text("Scan")
+            })
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: self.handleScan)
+            }
+        }
+    }
+    
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+        self.isShowingScanner = false
+        switch result {
+        case .success(let code):
+            let details = code.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+
+            self.prospects.people.append(person)
+        case .failure(let error):
+            print("Scanning failed")
         }
     }
 }
