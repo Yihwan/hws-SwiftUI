024edf2a0479e04f3d383bcee8133cb9d8b3345c
yihwan <hi@yihwan.kim>
2020-02-12 17:40:38 -0800

    add HotProspects

---
+++ HotProspects/HotProspects/models/Prospect.swift
@@ -0,0 +1,50 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Yihwan Kim on 2/10/20.
+//  Copyright © 2020 Yihwan Kim. All rights reserved.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    let id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    static let saveKey = "SavedData"
+    
+    @Published private(set) var people: [Prospect]
+
+    init() {
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                return
+            }
+        }
+
+        self.people = []
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+        }
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+}
